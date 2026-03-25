268b1f02f6ee125318345fa2316dbb65c15e49ba
orlando-IDA <orlandogoncalves8585@gmail.com>
2026-03-25 09:06:07 -0300

    feature: Vehicle model finished

---
+++ src/main/java/br/com/fiap/Shift/models/Vehicle.java
@@ -0,0 +1,19 @@
+package br.com.fiap.Shift.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Vehicle {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String manuFacturer;
+    private String model;
+    private Integer year;
+    private String licensePlate;
+    private float dailyPrice;
+}
