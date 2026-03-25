cbb4de196893337717ad3edbb516cd84572a7158
orlando-IDA <orlandogoncalves8585@gmail.com>
2026-03-25 10:00:15 -0300

    feature: Customer model finished

--- src/main/java/br/com/fiap/Shift/models/Customer.java
+++ src/main/java/br/com/fiap/Shift/models/Customer.java
@@ -1,5 +1,22 @@
 package br.com.fiap.Shift.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
 public class Customer {
-    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String driverLicense;
+    private String cpf;
+    private LocalDate expiryDate;
+    private Boolean active;
 }
+
