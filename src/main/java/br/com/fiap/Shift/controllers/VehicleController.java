336e301539b928355fd46b9b016973ee12c91437
orlando-IDA <orlandogoncalves8585@gmail.com>
2026-03-25 10:51:12 -0300

    feature: VehicleController finished.

--- src/main/java/br/com/fiap/Shift/controllers/VehicleController.java
+++ src/main/java/br/com/fiap/Shift/controllers/VehicleController.java
@@ -6,9 +6,9 @@ import br.com.fiap.Shift.repositories.VehicleRepository;
 import br.com.fiap.Shift.service.VehicleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +21,32 @@ public class VehicleController {
 
     @GetMapping
     public List<Vehicle> listAll() { return service.getAllVehicles();}
+
+    @PostMapping
+    public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.addVehicles(vehicle));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable Integer id) {
+        log.info("getVehicleById" + id);
+        return ResponseEntity.ok(service.getVehiclesById(id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteVehicleById(@PathVariable Integer id) {
+        log.info("deleteVehicleById" + id);
+        service.deleteVehicleById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Vehicle> updateVehicleById(@PathVariable Integer id, @RequestBody Vehicle vehicle) {
+        log.info("updateVehicleById" + id);
+        return ResponseEntity.ok(service.updateVehicle(id, vehicle));
+    }
 
     
 }
