42b8b9539710d324573b5dee5de06a59891aaa44
orlando-IDA <orlandogoncalves8585@gmail.com>
2026-03-25 10:37:23 -0300

    feature: VehicleController started.

--- src/main/java/br/com/fiap/Shift/service/VehicleService.java
+++ src/main/java/br/com/fiap/Shift/service/VehicleService.java
@@ -1,7 +1,7 @@
 package br.com.fiap.Shift.service;
 
 import br.com.fiap.Shift.models.Vehicle;
-import br.com.fiap.Shift.repository.VehicleRepository;
+import br.com.fiap.Shift.repositories.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
