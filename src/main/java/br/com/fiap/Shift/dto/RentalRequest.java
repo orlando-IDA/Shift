a7491cf8c737c8e8b796facd50f6da907d8fdced
orlando-IDA <orlandogoncalves8585@gmail.com>
2026-04-30 05:13:15 -0300

    feat: implemented RentalRequest with validation.

--- src/main/java/br/com/fiap/Shift/dto/RentalRequest.java
+++ src/main/java/br/com/fiap/Shift/dto/RentalRequest.java
@@ -1,20 +1,27 @@
 package br.com.fiap.Shift.dto;
 
+import br.com.fiap.Shift.models.Rental;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record RentalRequest(
-    @NotNull(message = "O ID do cliente é obrigatório")
+    @NotNull(message = "Field customer ID is mandatory.")
     Integer customerId,
 
-    @NotNull(message = "O ID do veículo é obrigatório")
+    @NotNull(message = "Field Vehicle ID is mandatory.")
     Integer vehicleId,
 
-    @NotNull(message = "A data de início é obrigatória")
-    @FutureOrPresent(message = "A data de início não pode ser no passado")
+    @NotNull(message = "Field Start Date is mandatory.")
+    @FutureOrPresent(message = "Start Future must be a Future or Present date.")
     LocalDateTime startDate,
 
     LocalDateTime endDate
 ) {
+    public Rental toEntity() {
+        Rental rental = new Rental();
+        rental.setStartDate(this.startDate);
+        rental.setEndDate(this.endDate);
+        return rental;
+    }
 }
